093807c103ec79ffd4d6bd9e6193839dd82ea006
Mitchell Lisle <mitchelllisle@mailbox.org>
2023-12-19 18:57:22 +1100

    refactor

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,7 +4,7 @@ package io.github.mitchelllisle
 def main(): Unit = {
   // Day One
   val dayOnePartOne = DayOnePartOne.fromFile("src/main/resources/day1.txt")
-  println(f"What is the sum of all of the calibration values? $dayOnePartOne")
+  println(s"What is the sum of all of the calibration values? $dayOnePartOne")
   
   val dayOnePartTwo = DayOnePartTwo.fromFile("src/main/resources/day1.txt")
   println(s"What is the sum of all of the calibration values? $dayOnePartTwo")
